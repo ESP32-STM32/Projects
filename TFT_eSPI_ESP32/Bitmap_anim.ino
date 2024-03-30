9b6eda584de1b6b95b0e9ce6300a7bc23a8674d7
ESP32&STM32 <159030581+ESP32-STM32@users.noreply.github.com>
2024-03-30 22:51:24 +0300

    Update and rename Bitmap.ino to Bitmap_anim.ino

---
+++ TFT_eSPI_ESP32/Bitmap_anim.ino
@@ -0,0 +1,27 @@
+#include <TFT_eSPI.h> // Библиотека для работы с дисплеем
+
+#include "battery.h" // Файл с массивом картинки
+#include "battery_anim.h" // Файл с массивом картинки
+
+TFT_eSPI tft = TFT_eSPI(); // Создание объекта для работы с дисплеем
+
+int count = 0;           // Батарейка
+static uint32_t tmrBAT;  // Батарейка
+
+void setup() {
+  tft.init();                 // Инициализация объекта дисплея
+  tft.setRotation(1);         // Горизонтальная ориентация экрана
+  tft.fillScreen(TFT_BLACK);  // Цвет фона экрана
+
+  // Вправо x, Вниз y, имя масива, ширина, высота, цвет.
+  tft.drawBitmap(0, 0, battery, 30, 30, TFT_RED); 
+}
+
+void loop() {
+  // if (millis() - tmrBAT >= 300) {  // Условие которое срабатывает через каждые 300 миллисекунд
+  //   tmrBAT = millis();
+  //   tft.drawBitmap(0, 0, battery_anim[count], 30, 30, TFT_GREEN, TFT_BLACK);  // Батарейка
+  //   count++;
+  //   if (count > 6) count = 0;
+  // }
+}
