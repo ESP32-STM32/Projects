fcf4f73472146b1bbbf340d9cc8bda44f4cac77b
ESP32&STM32 <159030581+ESP32-STM32@users.noreply.github.com>
2024-12-25 19:55:59 +0300

    Add files via upload

---
+++ WIFI.h
@@ -0,0 +1,253 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+#include "FS.h"
+#include "SPIFFS.h"
+#include "SD.h"
+#include "web.h"
+
+#define LOCAL_ON  // Подключатся к домашней сети
+//#define AP_ON     // Создать точку доступа
+
+#define LOCAL_SSID "default"       // Имя домашней WIFI сети
+#define LOCAL_PASS "12345678test"  // Пароль домашней WIFI сети
+
+#define AP_SSID "ESP32"     // Имя точки доступа
+#define AP_PASS "12345678"  // Пароль точки доступа
+
+const char *host = "esp32";  // http://esp32.local
+unsigned long tmrHandle;
+uint64_t usedBytes = 0;
+uint64_t totalBytes = 0;
+
+File fsUploadFile;
+String filesys = "SPIFFS";
+
+WebServer server(80);
+
+IPAddress PageIP(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+void handleDelete();
+void SystemSize();
+void handleFileList();
+void handleFileUpload();
+
+void init() {
+  Serial.begin(115200);
+
+#ifdef LOCAL_ON
+  WiFi.begin(LOCAL_SSID, LOCAL_PASS);
+#endif
+
+#ifdef AP_ON
+  WiFi.softAP(AP_SSID, AP_PASS);
+  delay(100);
+  WiFi.softAPConfig(PageIP, gateway, subnet);
+  delay(100);
+#endif
+  // if (!SD.begin()) {
+  //   Serial.println("Card Mount Failed");
+  //   return;
+  // }
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+  MDNS.begin(host);
+  server.on("/", HTTP_DELETE, handleDelete);
+  server.on("/getSize", HTTP_POST, SystemSize);
+  server.on("/getFiles", HTTP_POST, handleFileList);
+  server.on(
+    "/", HTTP_POST, []() {
+      server.send(200, "text/plain", "1");
+    },
+    handleFileUpload);
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", Web_page);
+  });
+  server.begin();
+  MDNS.addService("http", "tcp", 80);
+}
+
+void handleUP() {
+  if (millis() - tmrHandle >= 2) {
+    tmrHandle = millis();
+    server.handleClient();
+  }
+}
+
+void getFiles(fs::FS &fs) {
+  File root = fs.open("/");
+  String output = "[";
+  if (root.isDirectory()) {
+    File file = root.openNextFile();
+    while (file) {
+      if (output != "[") {
+        output += ',';
+      }
+      output += "{\"type\":\"";
+      output += (file.isDirectory()) ? "dir" : "file";
+      output += "\",\"name\":\"";
+      output += String(file.path()).substring(1);
+      output += "\"}";
+      file = root.openNextFile();
+    }
+  }
+  output += "]";
+  server.send(200, "text/json", output);
+}
+
+void handleFileList() {
+  getFiles(SPIFFS);
+  // if (filesys == "SPIFFS") {
+  //   getFiles(SPIFFS);
+  // } else if (filesys == "SD") {
+  //   getFiles(SD);
+  // }
+}
+
+void filesUpload(fs::FS &fs) {
+  HTTPUpload &upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    String filename = upload.filename;
+    if (!filename.startsWith("/")) {
+      filename = "/" + filename;
+    }
+
+    fsUploadFile = fs.open(filename, "w");
+    filename = String();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (fsUploadFile) {
+      fsUploadFile.write(upload.buf, upload.currentSize);
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile) {
+      fsUploadFile.close();
+    }
+  }
+}
+
+void handleFileUpload() {
+  filesUpload(SPIFFS);
+  // if (filesys == "SPIFFS") {
+  //   filesUpload(SPIFFS);
+  // } else if (filesys == "SD") {
+  //   filesUpload(SD);
+  // }
+}
+
+String SystemSize1(uint64_t bytes) {
+  if (bytes >= 1073741824) {
+    return String(float(bytes) / (1048576 * 1024)) + " ГБ";
+  } else if (bytes < 1073741824 && bytes >= 1048576) {
+    return String((float(bytes) / (1024 * 1024))) + " МБ";  // Мегабайты
+  } else if (bytes < 1048576 && bytes >= 1024) {
+    return String(bytes / 1024) + " КБ";  // Килобайты
+  } else if (bytes < 1024) {
+    return String(bytes) + " Б";  // Байты
+  } else return "0";
+}
+
+void returnFail(String msg) {
+  server.send(500, "text/plain", msg + "\r\n");
+}
+
+uint64_t map1(uint64_t x, uint64_t in_min, uint64_t in_max, uint64_t out_min, uint64_t out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void SystemSize() {
+  if (server.args() == 0) {
+    Serial.println(server.args());
+    Serial.println("BAD ARGS");
+    return returnFail("BAD ARGS");
+  }
+
+  filesys = server.arg(0);
+
+  usedBytes = SPIFFS.usedBytes();    // Занято
+  totalBytes = SPIFFS.totalBytes();  // Всего
+  // if (filesys == "SPIFFS") {
+  //   usedBytes = SPIFFS.usedBytes();    // Занято
+  //   totalBytes = SPIFFS.totalBytes();  // Всего
+  // } else if (filesys == "SD") {
+  //   usedBytes = SD.usedBytes();    // Занято
+  //   totalBytes = SD.totalBytes();  // Всего
+  // }
+
+  int val = map1(usedBytes, 0, totalBytes, 0, 100);
+
+  String res = "Занято: ";
+  res += SystemSize1(usedBytes);
+  res += " из: ";
+  res += SystemSize1(totalBytes);
+
+  String output = "{\"used\":\"";
+  output += res;
+  output += "\",\"total\":\"";
+  output += String(val);
+  output += "\"}";
+  server.send(200, "text/json", output);
+}
+
+void deleteRecursive(fs::FS &fs, String path) {
+  File file = fs.open((char *)path.c_str());
+  if (!file.isDirectory()) {
+    file.close();
+    fs.remove((char *)path.c_str());
+    return;
+  }
+
+  file.rewindDirectory();
+  while (true) {
+    File entry = file.openNextFile();
+    if (!entry) {
+      break;
+    }
+    String entryPath = path + "/" + entry.name();
+    if (entry.isDirectory()) {
+      entry.close();
+      deleteRecursive(fs, entryPath);
+    } else {
+      entry.close();
+      fs.remove((char *)entryPath.c_str());
+    }
+    yield();
+  }
+
+  fs.rmdir((char *)path.c_str());
+  file.close();
+}
+
+void handleDelete1(fs::FS &fs, String path) {
+  if (path == "/" || !fs.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    Serial.println("BAD PATH");
+    return;
+  }
+  deleteRecursive(fs, path);
+  server.send(200, "text/plain", "1");
+}
+
+void handleDelete() {
+  if (server.args() == 0) {
+    Serial.println(server.args());
+    Serial.println("BAD ARGS");
+    return returnFail("BAD ARGS");
+  }
+
+  String path = "/";
+  path += String(server.arg(0));
+
+  handleDelete1(SPIFFS, path);
+  // if (filesys == "SPIFFS") {
+  //   handleDelete1(SPIFFS, path);
+  // } else if (filesys == "SD") {
+  //   handleDelete1(SD, path);
+  // }
+}
---
+++ web.h
@@ -0,0 +1,475 @@
+const char Web_page[] PROGMEM = R"=====( 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <title>Файловый менеджер</title>
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" /> -->
+</head>
+
+<body>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+        }
+
+        .container {
+            width: 400px;
+            height: auto;
+            box-shadow: 0px 0px 40px #3b4456;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            /* justify-content: space-around; */
+            border-radius: 15px;
+            font-family: 'poppins', sans-serif;
+            padding: 5px;
+            /* background-color: hsla(0, 0%, 0%, .1);
+            backdrop-filter: blur(24px); */
+        }
+
+        .progressBar {
+            background: rgb(199, 193, 193);
+            width: 100%;
+            height: 10px;
+            margin-top: 5px;
+            border-radius: 5px;
+            overflow: hidden;
+        }
+
+        #progressBar {
+            height: 25px;
+            border-radius: 10px;
+        }
+
+        .fill {
+            background-color: #ff8800;
+            height: 100%;
+            width: 0%;
+        }
+
+        #line1 {
+            background-color: #ff8800;
+            height: 100%;
+            width: 0%;
+        }
+
+        .upload-form {
+            width: 100%;
+            height: 90px;
+        }
+
+        .form-upload__input {
+            width: 100%;
+            margin-top: 5px;
+        }
+
+        @keyframes fadeinout {
+            0% {
+                opacity: 1;
+            }
+
+            100% {
+                opacity: 0;
+            }
+        }
+
+        .form-upload__input::file-selector-button {
+            font-size: 15px;
+            min-width: 50%;
+            margin-right: 10px;
+            height: 25px;
+            border: none;
+            border-radius: 10px;
+            background-color: #ff8800;
+            cursor: pointer;
+            transition: background-color 0.2s linear;
+        }
+
+        .form-upload__input::file-selector-button:hover {
+            background-color: #00ff55;
+            cursor: pointer;
+        }
+
+        .form-upload__submit {
+            font-size: 15px;
+            width: 100%;
+            margin-top: 5px;
+            height: 25px;
+            border-radius: 10px;
+            border: none;
+            color: #000000;
+            transition: background-color 0.2s linear;
+            background-color: #ff8800;
+        }
+
+        .form-upload__submit:hover {
+            background-color: #00ff55;
+            cursor: pointer;
+        }
+
+        .form-upload__status {
+            top: 0;
+            padding-left: 5px;
+            height: 25px;
+            display: flex;
+            align-items: center;
+            position: absolute;
+            user-select: none;
+        }
+
+        .radio {
+            border-radius: 10px;
+            border: 2px solid #ff8800;
+            height: 25px;
+            display: flex;
+            position: relative;
+            overflow: hidden;
+            user-select: none;
+        }
+
+        .radio label {
+            height: 25px;
+            min-width: 70px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+        }
+
+        .radio input:nth-child(1):checked~span {
+            transform: translateX(0%);
+        }
+
+        .radio input:nth-child(3):checked~span {
+            transform: translateX(100%);
+        }
+
+        .radio span {
+            position: absolute;
+            background-color: #ff8800;
+            width: 70px;
+            height: 29px;
+            color: white;
+            z-index: -1;
+            transition: 0.1s;
+        }
+
+        .containerFiles {
+            width: 100%;
+            height: auto;
+            display: block;
+        }
+
+        .stroc {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .fileName {
+            margin-top: 5px;
+            margin-right: 5px;
+            overflow: hidden;
+            height: 25px;
+            display: flex;
+            align-items: center;
+            justify-content: left;
+            border-left: 6px solid #ff8800;
+            border-right: 6px solid #ff8800;
+            border-radius: 10px;
+            width: 100%;
+            background-color: #ff8800;
+        }
+
+        .fill1 {
+            visibility: hidden;
+            margin-top: 0px;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255, 255, 255, .30);
+        }
+
+        .progress {
+            /* display: none; */
+            z-index: 1;
+            margin-top: 5px;
+            position: absolute;
+            width: 100%;
+            height: 25px;
+            border-radius: 10px;
+            background-image: linear-gradient(-45deg,
+                    #d30505 0%,
+                    #d30505 25%,
+                    transparent 25%,
+                    transparent 50%,
+                    #d30505 50%,
+                    #d30505 75%,
+                    transparent 75%,
+                    transparent 100%);
+            background-size: 25px;
+            animation: progress 1s linear infinite;
+        }
+
+        @keyframes progress {
+            to {
+                background-position: 25px;
+            }
+        }
+
+        .btn {
+            margin-top: 5px;
+            height: 25px;
+            background-color: #ff9100;
+            border: none;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+    </style>
+
+    <div class="container" id="container">
+        <div class="radio">
+            <input type="radio" id="radio" style="display: none;" name="radio" onclick="fanc()">
+            <label for="radio">SPIFFS</label>
+            <input type="radio" id="radio1" style="display: none;" name="radio" onclick="fanc()">
+            <label for="radio1">SPIFFS</label>
+            <span></span>
+        </div>
+        <div style="margin-top: 5px;" id="used">Объем памяти</div>
+        <div class="progressBar">
+            <div class="fill" id="line"></div>
+        </div>
+        <div class="upload-form">
+            <form id="uploadForm" method="post" enctype="multipart/form-data">
+                <input class="form-upload__input" type="file" id="uploadForm_File">
+                <input class="form-upload__submit" type="submit" value="Загрузить файл">
+                <div style="position: relative; z-index: 1;">
+                    <div class="progressBar" id="progressBar">
+                        <div class="fill" id="line1"></div>
+                    </div>
+                    <div class="form-upload__status" id="upload_data">
+                        <span id="uploadForm_Status">Статус</span>
+                        <span id="uploadForm_Size"></span>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+    <div class="fill1" id="fill"></div>
+    <script>
+        // document.getElementById('file').onchange = function () {
+        //     this.setAttribute('style', 'animation: fadeinout 0.5s linear;')
+        // };
+        //testloadValues();
+        var typefilesys = "SPIFFS";
+        getSize("SPIFFS", true);
+        // loadValues();
+        function loadValues() {
+            if (document.getElementById('err')) {
+                var ttt = document.getElementById('err')
+                ttt.remove()
+            }
+            const cont = document.getElementById('container');
+            const test = document.createElement('div');
+            test.className = "containerFiles"
+            test.id = "err"
+            cont.append(test);
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var i = 0;
+                    var myObj = JSON.parse(this.responseText);
+                    while (i < myObj.length) {
+                        if (myObj[i].type == "file") {
+                            let div = document.createElement('div');
+                            div.id = myObj[i].name
+                            div.setAttribute('style', 'position: relative;')
+                            div.className = "stroc"
+                            div.innerHTML = '<span class="fileName">' + myObj[i].name + '</span><button class="btn" data-name="' + myObj[i].name + '">Удалить</button>';
+                            test.append(div);
+                        }
+                        i++;
+                    }
+                }
+            };
+            xhttp.open("POST", "getFiles", true);
+            xhttp.send();
+
+            const removeHandler = event => {
+                if (!event.target.dataset.name) {
+                    return
+                }
+                const { name } = event.target.dataset
+                // --------------------------------------------
+                let fill = document.getElementById('fill')
+                fill.setAttribute('style', 'visibility: visible; cursor: not-allowed;')
+                let cont = document.getElementById(name)
+                let prog = document.createElement('div')
+                prog.id = "prog"
+                prog.className = "progress"
+                cont.append(prog)
+                // --------------------------------------------
+                httpDelete(name)
+            }
+            test.addEventListener('click', removeHandler)
+        };
+        function testloadValues() {
+            const cont = document.getElementById('container');
+            const test = document.createElement('div');
+            test.className = "containerFiles"
+            test.id = "err1"
+            cont.append(test);
+            // document.body.append(test);
+            var i = 0;
+            const json = '[{"type":"file","name":"edit423423423423423423424312312312312433234234234234234.htm.gz"},{"type":"file","name":"favicon.ico"},{"type":"file","name":"graphs.js.gz"},{"type":"file","name":"index.htm"}]';
+            var myObj = JSON.parse(json);
+            while (i < myObj.length) {
+                if (myObj[i].type == "file") {
+                    let div = document.createElement('div');
+                    div.id = myObj[i].name
+                    div.setAttribute('style', 'position: relative;')
+                    div.className = "stroc"
+                    div.innerHTML = '<span class="fileName">' + myObj[i].name + '</span><button class="btn" data-name="' + myObj[i].name + '">Удалить</button>';
+                    test.append(div);
+                }
+                i++;
+            }
+            const removeHandler = event => {
+                if (!event.target.dataset.name) {
+                    return
+                }
+                const { name } = event.target.dataset
+                // --------------------------------------------
+                let fill = document.getElementById('fill')
+                fill.setAttribute('style', 'visibility: visible; cursor: not-allowed;')
+                let cont = document.getElementById(name)
+                let prog = document.createElement('div')
+                prog.id = "prog"
+                prog.className = "progress"
+                cont.append(prog)
+                // --------------------------------------------
+                httpDelete(name)
+            }
+            test.addEventListener('click', removeHandler)
+        };
+
+        var toggle = false;
+        function fanc() {
+            if (toggle && radio.checked) {
+                getSize("SPIFFS", true)
+                // loadValues();
+                toggle = false;
+                typefilesys = "SPIFFS";
+            } else if (!toggle && radio1.checked) {
+                getSize("SPIFFS", true)
+                // loadValues();
+                typefilesys = "SPIFFS";
+                toggle = true;
+            }
+        }
+        function getSize(filesystem, choice) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var myObj = JSON.parse(this.responseText);
+                    document.getElementById("used").innerHTML = myObj.used;
+                    document.getElementById('line').style.width = myObj.total.toString() + '%';
+                    if (choice) {
+                        loadValues();
+                    }
+                }
+            };
+            var formData = new FormData();
+            formData.append("path", filesystem);
+            xhttp.open("POST", "getSize", true);
+            xhttp.send(formData);
+        }
+
+        const BYTES_IN_MB = 1048576
+
+        const form = document.getElementById('uploadForm')
+        const fileInput = document.getElementById('uploadForm_File')
+        const sizeText = document.getElementById('uploadForm_Size')
+        const statusText = document.getElementById('uploadForm_Status')
+        const progressBar = document.getElementById('line1')
+
+        fileInput.addEventListener('change', function () {
+            const file = this.files[0]
+            if (file.size > 10 * BYTES_IN_MB) {
+                alert('Принимается файл до 10 МБ')
+                this.value = null
+            }
+        });
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            const fileToUpload = fileInput.files[0]
+            const formSent = new FormData()
+            const xhr = new XMLHttpRequest()
+
+            if (fileInput.files.length > 0) {
+                let fill = document.getElementById('fill')
+                fill.setAttribute('style', 'visibility: visible; cursor: not-allowed;')
+                formSent.append('file', fileToUpload)
+                // собираем запрос и подписываемся на событие progress
+                xhr.upload.addEventListener('progress', progressHandler, false)
+                xhr.addEventListener('load', loadHandler, false)
+                xhr.open('POST', '/')
+                xhr.send(formSent)
+            } else {
+                alert('Сначала выберите файл')
+            }
+            return false
+        });
+
+        function progressHandler(event) {
+            // считаем размер загруженного и процент от полного размера
+            const loadedMb = (event.loaded / BYTES_IN_MB).toFixed(1)
+            const totalSizeMb = (event.total / BYTES_IN_MB).toFixed(1)
+            const percentLoaded = Math.round((event.loaded / event.total) * 100)
+
+            progressBar.style.width = percentLoaded + '%'
+            sizeText.textContent = ` ${loadedMb} МБ из ${totalSizeMb} МБ`
+            statusText.textContent = `Загружено ${percentLoaded}% | `
+        }
+
+        function loadHandler(event) {
+            sizeText.textContent = ``
+            statusText.textContent = `Загрузка завершена`
+            //statusText.textContent = event.target.responseText
+            progressBar.style.width = '0%'
+            let fill = document.getElementById('fill')
+            fill.setAttribute('style', 'visibility: hidden; cursor: default;')
+            document.getElementById('uploadForm_File').value = '';
+            getSize(typefilesys, true);
+            // loadValues();
+        }
+
+        function httpDelete(filename) {
+            xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    const cont = document.getElementById(filename)
+                    cont.remove()
+                    let fill = document.getElementById('fill')
+                    fill.setAttribute('style', 'visibility: hidden; cursor: default;')
+                    getSize(typefilesys, false);
+                    // loadValues();
+                }
+            };
+            var formData = new FormData();
+            formData.append("path", filename);
+            xmlHttp.open("DELETE", "/");
+            xmlHttp.send(formData);
+        }
+    </script>
+
+</body>
+
+</html>
+)=====";
